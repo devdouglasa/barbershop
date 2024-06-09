b7c792e3396820244d9ba600738e307a778f2601
devdouglasa <douglasalisson27@gmail.com>
2024-06-09 14:37:43 -0300

    feat: adicionado pagina de Login e Cadastrar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:barbershop/pages/CadastroLogin.dart';
 import 'package:barbershop/pages/LoginPage.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const CadastroLogin(),
     );
   }
 }
