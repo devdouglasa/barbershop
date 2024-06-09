7e1debd461a80e8aa35dbffc1fbf6d16de6ea2a7
devdouglasa <douglasalisson27@gmail.com>
2024-06-09 13:54:46 -0300

    feat: adicionado botão de login da LoginPage.dart

--- lib/components/Login/buttonLogin.dart
+++ lib/components/Login/buttonLogin.dart
@@ -6,16 +6,16 @@ class ButtonLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 20, left: 25, right: 25),
+      padding: const EdgeInsets.only(top: 40, left: 25, right: 25),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.blue[700],
-          fixedSize: const Size(500, 50),
+          fixedSize: const Size(500, 60),
           elevation: 0,
         ),
         onPressed: () {},
         child: const Text(
-          "LOGIN",
+          "ENTRAR",
           style: TextStyle(color: Colors.white, letterSpacing: 2),
         ),
       ),
