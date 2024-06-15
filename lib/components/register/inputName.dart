679039bbb1646839f0dd12a9d60aed7843c44114
devdouglasa <douglasalisson27@gmail.com>
2024-06-15 02:38:05 -0300

    feat: criado campo de input do nome

---
+++ lib/components/register/inputName.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class InputName extends StatelessWidget {
+  const InputName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25, right: 25),
+      child: TextFormField(
+        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(20),
+            fillColor: Colors.lightBlueAccent,
+            filled: true,
+            prefixIcon: Padding(
+              padding: EdgeInsets.only(left: 15, right: 10),
+              child: Icon(Icons.person),
+            ),
+            label: Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text("Digite seu nome"),
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.all(Radius.circular(25))
+            )
+        ),
+      ),
+    );
+  }
+}
